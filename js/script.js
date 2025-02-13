310005f4e5612d0a6b4e1f7b7fc4dac72dc46509
deadflowers <deadflowers@users.noreply.github.com>
2025-02-12 16:56:02 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -345,9 +345,6 @@ function insertItemPortionName(html,
 
 
 global.$dc = dc;
-  
-})(window);
-
 
 (function(){var style=document.createElement('style'),styleContent=document.createTextNode('*{box-sizing:border-box;} p,footer,header,a:link,article,video,input,h1,hr,h3{
    animation-delay:1.5s;  animation:fly-in-from-right 0.93s 1s ease both;
@@ -358,3 +355,6 @@ global.$dc = dc;
 @keyframes fly-in-from-right{from{transform:translateY(12rem) rotate(-360deg);opacity:0;}}
 button,.btn{opacity:0;-webkit-transition:opacity 0.1s ease-in-out; transition:opacity 0.1s ease-in-out;-webkit-transform:translateZ();}.wrap{backface-visibility:hidden;font-weight:400;letter-spacing:0px;position:relative;transition-delay:0s;transition-duration:0.3s;transition-property:transform;transition-timing-function:cubic-bezier(0.42,0,0.58,1);}');style.appendChild(styleContent);var mexMeHead=document.getElementsByTagName('head');mexMeHead[0].appendChild(style);})();
 
+
+  
+})(window);
